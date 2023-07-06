731215a3751623ca47301e981eaa08679ffac309
Nathalie Senechal <nathalie.senechal@mailbox.tu-dresden.de>
2023-07-06 10:55:31 +0200

    continue funxtionality for content

---
+++ Assets/Scripts/Logic/COntinue.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class COntinue : MonoBehaviour
+{
+    [SerializeField] private GameObject scrollContent;
+    [SerializeField] private GameObject scrollbar;
+    [SerializeField] private int contentIndex = 1;
+    [SerializeField] private float scrollValue = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contentIndex = 1;
+        HideScrollContent();
+    }
+
+    public void ShowNextScrollContentElement()
+    {
+        if (contentIndex < scrollContent.transform.childCount)
+        {
+            GetScrollContentElement(contentIndex).SetActive(true);
+            contentIndex++;
+            scrollbar.GetComponent<Scrollbar>().value = scrollValue;
+        }
+     
+    }
+    private GameObject GetScrollContentElement(int index)
+    {
+
+        return scrollContent.transform.GetChild(index).gameObject;
+    }
+    private void HideScrollContent()
+    {
+        for(int i = 1; i < scrollContent.transform.childCount; i++)
+        {
+            GetScrollContentElement(i).SetActive(false);
+        }
+    }
+}
